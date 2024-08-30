b90c46f3d041775b7e2a273af64194077a06c16a
Abhay Tripathi <abhaytripathi0203@gmail.com>
2024-08-31 01:18:29 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://apnagurukul-ui.vercel.app/",
+    origin: "https://apnagurukul-ui.vercel.app/", 
     credentials: true,
   })
 );
