e601d7476bdadbc6a5cd97f4c1ecf9c679918483
Abhay Tripathi <abhaytripathi0203@gmail.com>
2024-08-31 01:12:49 +0530

    changes

--- Frontend/src/services/apiConnector.js
+++ Frontend/src/services/apiConnector.js
@@ -12,7 +12,7 @@ export const  apiConnector = (method, url, bodyData, headers, params) => {
         method: `${method}`,
         url: `${url}`,
         data: bodyData ? bodyData : null,
-        headers: headers ? headers : { "Access-Control-Allow-Origin": "*" },
+        headers: headers ? headers : { "Access-Control-Allow-Origin": "https://apnagurukul-ui.vercel.app/" },
         params: params ? params : null,
     });
 }
--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://apnagurukul-ui.vercel.app/",
     credentials: true,
   })
 );
